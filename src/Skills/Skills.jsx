521656f0bce950565d49d31711cdfcaec88ff1d6
rajesh <chagantipati.rajesh09@gmail.com>
2024-06-16 13:52:14 +0530

    skill images added

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -1,14 +1,40 @@
 import "./Skills.css";
+import java from "./Images/java.svg";
+import springBoot from "./Images/spring-boot.png";
+import javaScript from "./Images/javaScript.png";
+import reactJs from "./Images/reactJs.png";
+import git from "./Images/git.png";
+import sqlServer from "./Images/sqlServer.svg";
+import mongoDb from "./Images/mongoDb.png";
+import postMan from "./Images/postMan.png";
+import html from "./Images/html.png";
+import css from "./Images/css.png";
 
 function Skills() {
-    const skills = ["Java", "Spring Boot","JavaScript", "React JS","Version Control System (GIT)", "SQL Server", "MongoDB", "Postman", "HTML", "CSS"];
-    const displaySkills = skills.map((skill) =><li>{skill}</li>)
-    return (
-        <div>
-            <h1>Skills</h1>
-            <ul>{displaySkills}</ul>
-        </div>
-    );
+  const skills = [
+    { key: 1, skill: "Java", image: java },
+    { key: 2, skill: "Spring Boot", image: springBoot },
+    { key: 3, skill: "JavaScript", image: javaScript },
+    { key: 4, skill: "React JS", image: reactJs },
+    { key: 5, skill: "Version Control System (GIT)", image: git },
+    { key: 6, skill: "SQL Server", image: sqlServer },
+    { key: 7, skill: "MongoDB", image: mongoDb },
+    { key: 8, skill: "Postman", image: postMan },
+    { key: 9, skill: "HTML", image: html },
+    { key: 10, skill: "CSS", image: css },
+  ];
+  const displaySkills = skills.map(({ key, skill, image }) => 
+    (
+    <li key={key}>
+      <img src={image} alt={skill} className="skill-image" />
+    </li>
+  ));
+  return (
+    <div className="skills">
+      <h1>My Skills</h1>
+      <ul>{displaySkills}</ul>
+    </div>
+  );
 }
 
 export default Skills;
